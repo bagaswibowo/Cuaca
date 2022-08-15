a16c1418b622b88dfc148da5e215dbfde2f774d4
bagaswibowo <bagaswibowo121@gmail.com>
2022-08-15 22:48:51 +0700

    Initial Commit

---
+++ Cuaca/CuacaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuacaApp.swift
+//  Cuaca
+//
+//  Created by Bagas Wibowo on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CuacaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
